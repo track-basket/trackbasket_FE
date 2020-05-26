97207ae53be93c8303f0e9f44bf2bc7cb08ca6f9
monstaro <codysmith2814@gmail.com>
2020-05-26 17:16:25 -0600

    Setting up Navigation
    Co-authored-by: Ed Stoner <edwardlstoner@gmail.com>

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import LoginModal from './screens/LoginModal';
+import AtRiskTabs from './screens/AtRiskTabs';
+const RootStack = createStackNavigator();
+const MainStack = createStackNavigator();
 
-export default function App() {
+const MainStackScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen name="AtRiskTabs" component={AtRiskTabs} />
+    </MainStack.Navigator>
   );
-}
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator mode="modal">
+        <RootStack.Screen
+          name="Main"
+          component={MainStackScreen}
+          options={{ headerShown: false }}
+        />
+        <RootStack.Screen name="LoginModal" component={LoginModal} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +40,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
