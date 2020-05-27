b77932909795d1ea9fa208bed5cb75fcffcee5c4
monstaro <codysmith2814@gmail.com>
2020-05-26 19:11:10 -0600

    Adds logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
 
 const Logo = () => {
   return (
-    <View>
+    <View style={styles.logoContainer}>
       <Image
         style={styles.logo}
         source={require('../assets/trackbasket.png')}
       />
+      <Text style={styles.logoText}>Trackbasket</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  logoContainer: {
+    alignItems: 'center',
+  },
   logo: {
     marginTop: 20,
     height: 99,
     width: 105,
+  },
+  logoText: {
+    color: '#59DE7E',
+    fontFamily: 'HelveticaNeue-Bold',
+    fontSize: 24,
   },
 });
 
