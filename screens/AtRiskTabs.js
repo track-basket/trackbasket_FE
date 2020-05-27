422627ecd57637cd4db0744cb63bb2dcb641b7e6
monstaro <codysmith2814@gmail.com>
2020-05-26 21:49:10 -0600

    Inserts icons

--- screens/AtRiskTabs.js
+++ screens/AtRiskTabs.js
@@ -1,10 +1,13 @@
-import { MaterialIcons } from 'react-native-vector-icons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Cart from './Cart';
 import Shop from './Shop';
 
 const Tab = createBottomTabNavigator();
+
+const shopBasket = <Icon.Button name="shopping_basket" />;
 
 const AtRiskTabs = () => {
   return (
@@ -14,18 +17,45 @@ const AtRiskTabs = () => {
         name="Shop"
         options={{
           tabBarLabel: 'Shop',
-          tabBarIcon: ({ color, size }) => {
-            <MaterialIcons name="home" color={color} size={size} />;
+          tabBarIcon: () => {
+            return (
+              <Icon.Button
+                name="shopping-basket"
+                style={styles.tabIcon}
+                size={20}
+                color={'#3d70ff'}
+                component={Shop}
+              />
+            );
           },
         }}
       />
       <Tab.Screen
         component={Cart}
         name="Cart"
-        options={{ tabBarLabel: 'Cart' }}
+        options={{
+          tabBarLabel: 'Cart',
+          tabBarIcon: () => {
+            return (
+              <Icon.Button
+                name="shopping-cart"
+                style={styles.tabIcon}
+                size={20}
+                color={'#3d70ff'}
+              />
+            );
+          },
+        }}
       />
     </Tab.Navigator>
   );
 };
 
+const styles = StyleSheet.create({
+  tabIcon: {
+    color: '#3d70ff',
+    backgroundColor: '#FFF',
+    paddingLeft: 10,
+  },
+});
 export default AtRiskTabs;
