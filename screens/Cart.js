02f6bb9259d753a1505ba5c77b1822271fb24447
monstaro <codysmith2814@gmail.com>
2020-05-27 17:32:00 -0600

    Cart items display

--- screens/Cart.js
+++ screens/Cart.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import UserContext from '../user-context';
+import GroceryItem from '../components/GroceryItem';
 
-const Cart = ({ navigation, route }) => {
+const Cart = () => {
+  const { user, cart } = useContext(UserContext);
+  console.log(user, cart);
   return (
     <View>
       <Text>Cart</Text>
+      {cart.map((item) => {
+        return (
+          <GroceryItem
+            upc={item.upc}
+            aisleNumber={item.aisleNumber}
+            description={item.description}
+            image_url={item.image_url}
+            price={item.price}
+          />
+        );
+      })}
     </View>
   );
 };
