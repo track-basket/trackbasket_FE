8df7f67c01ed55c5adcee8acfa530bdce6d652d2
Edward Stoner <edwardlstoner@gmail.com>
2020-05-26 21:40:05 -0600

    Add Provider

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/Home';
 import LoginModal from './screens/LoginModal';
 import AtRiskTabs from './screens/AtRiskTabs';
+import { UserProvider } from './user-context';
+
 const RootStack = createStackNavigator();
 const MainStack = createStackNavigator();
 
@@ -18,17 +20,32 @@ const MainStackScreen = () => {
 };
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [cart, setCart] = useState(['item 1', 'item 2', 'apples', 'bananas']);
+  const addToCart = (newItem) => setCart([...cart, newItem]);
+  const setNewUser = (user) => setUser(user);
+  const [location, setLocation] = useState(null);
   return (
-    <NavigationContainer style={styles.container}>
-      <RootStack.Navigator mode="modal">
-        <RootStack.Screen
-          name="Main"
-          component={MainStackScreen}
-          options={{ headerShown: false }}
-        />
-        <RootStack.Screen name="LoginModal" component={LoginModal} />
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <UserProvider
+      value={{
+        user,
+        cart,
+        addToCart,
+        setNewUser,
+        location,
+      }}
+    >
+      <NavigationContainer style={styles.container}>
+        <RootStack.Navigator mode="modal">
+          <RootStack.Screen
+            name="Main"
+            component={MainStackScreen}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen name="LoginModal" component={LoginModal} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </UserProvider>
   );
 };
 
