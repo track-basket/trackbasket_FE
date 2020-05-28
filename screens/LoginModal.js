5f7282f6ea1e609ca71599c531919d1de2045e9e
monstaro <codysmith2814@gmail.com>
2020-05-27 20:39:22 -0600

    Adds scrollview

--- screens/LoginModal.js
+++ screens/LoginModal.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   TextInput,
   Alert,
+  ScrollView,
 } from 'react-native';
 import UserContext from '../user-context';
 import { TextField } from '../components/Form';
@@ -60,49 +61,51 @@ const LoginModal = ({ navigation }) => {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.containerInner}>
-        {!user && (
-          <Text style={styles.introtext}>
-            Sign up to order groceries and have them delivered by a volunteer
-          </Text>
-        )}
-        {user && <Text style={styles.introtext}>Update your info</Text>}
-        <TextField
-          label="Name"
-          placeholder="Name"
-          onChangeText={setName}
-          value={name}
-        />
-        <TextField
-          label="Delivery address"
-          placeholder="Name"
-          onChangeText={setAddress}
-          value={address}
-        />
-        <TextField
-          label="City"
-          placeholder="City"
-          onChangeText={setCity}
-          value={city}
-        />
-        <TextField
-          label="Zip code"
-          placeholder="Zip Code"
-          onChangeText={setZip}
-          value={zip}
-        />
-        <TextField
-          label="Phone number"
-          placeholder="Phone number"
-          onChangeText={setPhone}
-          value={phone}
-        />
-        <View style={styles.btnContainer}>
-          <Button onPress={handleSubmit} text="SUBMIT" />
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.containerInner}>
+          {!user && (
+            <Text style={styles.introtext}>
+              Sign up to order groceries and have them delivered by a volunteer
+            </Text>
+          )}
+          {user && <Text style={styles.introtext}>Update your info</Text>}
+          <TextField
+            label="Name"
+            placeholder="Name"
+            onChangeText={setName}
+            value={name}
+          />
+          <TextField
+            label="Delivery address"
+            placeholder="Name"
+            onChangeText={setAddress}
+            value={address}
+          />
+          <TextField
+            label="City"
+            placeholder="City"
+            onChangeText={setCity}
+            value={city}
+          />
+          <TextField
+            label="Zip code"
+            placeholder="Zip Code"
+            onChangeText={setZip}
+            value={zip}
+          />
+          <TextField
+            label="Phone number"
+            placeholder="Phone number"
+            onChangeText={setPhone}
+            value={phone}
+          />
+          <View style={styles.btnContainer}>
+            <Button onPress={handleSubmit} text="SUBMIT" />
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
