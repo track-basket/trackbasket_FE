a0fef5c046e7b3401907588c6b1392f6c17e5722
monstaro <codysmith2814@gmail.com>
2020-05-27 18:02:46 -0600

    Brings in toggleCart functionality

--- screens/Cart.js
+++ screens/Cart.js
@@ -4,24 +4,35 @@ import UserContext from '../user-context';
 import GroceryItem from '../components/GroceryItem';
 
 const Cart = () => {
-  const { user, cart } = useContext(UserContext);
-  console.log(user, cart);
-  return (
-    <View>
-      <Text>Cart</Text>
-      {cart.map((item) => {
-        return (
-          <GroceryItem
-            upc={item.upc}
-            aisleNumber={item.aisleNumber}
-            description={item.description}
-            image_url={item.image_url}
-            price={item.price}
-          />
-        );
-      })}
-    </View>
-  );
+  const { removeFromCart, addToCart, cart } = useContext(UserContext);
+  const toggleCartItem = (upc) => {
+    let selectedItem = cart.find((item) => item.upc === upc);
+    addToCart(selectedItem);
+    if (cart.find((item) => item.upc === upc)) {
+      removeFromCart(selectedItem);
+    }
+  };
+  if (cart.length) {
+    return (
+      <View>
+        <Text>Cart</Text>
+        {cart.map((item) => {
+          return (
+            <GroceryItem
+              upc={item.upc}
+              aisleNumber={item.aisleNumber}
+              description={item.description}
+              image_url={item.image_url}
+              price={item.price}
+              clickHandler={toggleCartItem}
+            />
+          );
+        })}
+      </View>
+    );
+  } else {
+    return <Text>Your Cart Is Empty</Text>;
+  }
 };
 
 const styles = StyleSheet.create({
