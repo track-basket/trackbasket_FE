b4318afd22c69643cc5536b7b5fe17fb779293df
Edward Stoner <edwardlstoner@gmail.com>
2020-05-27 20:51:16 -0600

    Add styling

--- screens/Shop.js
+++ screens/Shop.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-shadow */
 import React, { useState, useContext } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 import GroceryItem from '../components/GroceryItem';
 import UserContext from '../user-context';
 
@@ -41,26 +41,28 @@ const Shop = () => {
     }
   };
   return (
-    <View>
-      <TextInput
-        placeholder="Search Items..."
-        style={styles.searchBar}
-        onChangeText={(text) => setText(text)}
-        onSubmitEditing={() => console.log(text)}
-      />
-      {items.map((item) => {
-        return (
-          <GroceryItem
-            upc={item.upc}
-            aisleNumber={item.aisleNumber}
-            description={item.description}
-            image_url={item.image_url}
-            price={item.price}
-            clickHandler={toggleCartItem}
-            key={item.upc}
-          />
-        );
-      })}
+    <View style={styles.container}>
+      <View style={styles.innercontainer}>
+        <TextInput
+          placeholder="Search Items..."
+          style={styles.searchBar}
+          onChangeText={(text) => setText(text)}
+          onSubmitEditing={() => console.log(text)}
+        />
+        {items.map((item) => {
+          return (
+            <GroceryItem
+              upc={item.upc}
+              aisleNumber={item.aisleNumber}
+              description={item.description}
+              image_url={item.image_url}
+              price={item.price}
+              clickHandler={toggleCartItem}
+              key={item.upc}
+            />
+          );
+        })}
+      </View>
     </View>
   );
 };
@@ -70,7 +72,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  innercontainer: {
+    width: 350,
   },
   searchBar: {
     borderWidth: 1,
