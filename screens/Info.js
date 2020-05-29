8bf1c9f0a9e62ff4e6a6ff12934b6b690d14a861
Edward Stoner <edwardlstoner@gmail.com>
2020-05-28 20:39:45 -0600

    Create info component

---
+++ screens/Info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Info = () => {
+  return (
+    <View>
+      <Text>Info</Text>
+    </View>
+  );
+};
+
+export default Info;
