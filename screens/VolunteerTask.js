e474deeafb936dd2a2a30be0d818791c235e8b40
monstaro <codysmith2814@gmail.com>
2020-05-29 16:00:33 -0600

    Init commit

---
+++ screens/VolunteerTask.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const VolunteerTask = () => {
+  return (
+    <View>
+      <Text>Task Details</Text>
+      <Text>Name: </Text>
+      <Text>Joe Smith</Text>
+      <Text>Delivery Address: </Text>
+      <Text>123 Main Street, Denver CO 80203</Text>
+      <Text>Store: </Text>
+      <Text>King Soopers, 333 Speer Blvd</Text>
+      <Text>Ordered at: 1:21 PM May 21</Text>
+      <Text>Items: </Text>
+      <Text>29</Text>
+    </View>
+  );
+};
+
+export default VolunteerTask;
