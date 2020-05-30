14f6726237fbf25c884bb1b26b97d4f04be10162
monstaro <codysmith2814@gmail.com>
2020-05-30 14:26:37 -0600

    Moves dummy data

--- screens/VolunteerHome.js
+++ screens/VolunteerHome.js
@@ -11,7 +11,52 @@ import {
 } from 'react-native';
 import Logo from '../components/Logo';
 import TimeOfDay from '../components/TimeOfDay';
+// import { AsyncStorage } from 'react-native';
+ 
+function calcCrow(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km
+  var dLat = toRad(lat2 - lat1);
+  var dLon = toRad(lon2 - lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
 
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) {
+  return (Value * Math.PI) / 180;
+}
+
+// const storeData = async () => {
+//   try {
+//     await AsyncStorage.setItem(
+//       '@MySuperStore:key',
+//       JSON.stringify({
+//         lists: ['list1', 'list2', 'list3'],
+//       }),
+//     );
+//   } catch (error) {
+//     // Error saving data
+//   }
+// };
+
+// const retrieveData = async () => {
+//   try {
+//     const value = await AsyncStorage.getItem('@MySuperStore:key');
+//     if (value !== null) {
+//       // We have data!!
+//       console.log(JSON.parse(value));
+//     }
+//   } catch (error) {
+//     // Error retrieving data
+//   }
+// };
 const VolunteerHome = ({ navigation }) => {
   const { volunteer, assignedLists } = useContext(VolunteerContext);
   return (
--- screens/VolunteerShop.js
+++ screens/VolunteerShop.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+/* eslint-disable no-shadow */
+import React, { useState, useContext } from 'react';
+import { StyleSheet, View, TextInput, ScrollView } from 'react-native';
+import VolunteerContext from '../volunteer-context';
 
 const VolunteerShop = () => {
+  const { volunteer, assignedLists, setAssignedLists } = useContext(
+    VolunteerContext,
+  );
+  console.log(
+    'b',
+    assignedLists
+      .map((list) =>
+        list.items.map((item) => {
+          return item.upc;
+        }),
+      )
+      .flat(),
+  );
   return (
-    <View>
-      <Text>Volunteer Shop</Text>
+    <View style={styles.container}>
+      <View style={styles.innercontainer}>
+        {/* <ScrollView>
+          {assignedLists.items.map((item) => {
+            return item.upc;
+          })}
+        </ScrollView> */}
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  innercontainer: {
+    width: 350,
+  },
+  searchBar: {
+    borderWidth: 1,
+    fontSize: 18,
+    height: 50,
+    marginTop: 10,
+    marginBottom: 40,
+    padding: 5,
+  },
+});
 
 export default VolunteerShop;
