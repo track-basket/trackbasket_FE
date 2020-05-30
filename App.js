c3b18c718ba1101971be9dd7bd6af424aa3a40c1
monstaro <codysmith2814@gmail.com>
2020-05-30 12:23:35 -0600

    Removes function

--- App.js
+++ App.js
@@ -114,7 +114,6 @@ const App = () => {
         submitOrder,
         assignedLists,
         setAssignedLists,
-        updateListStatus,
         location,
         setLocation,
         allLists,
