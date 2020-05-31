d2dad59325676235a50781dceeb7d63561d0219f
Edward Stoner <edwardlstoner@gmail.com>
2020-05-30 23:20:13 -0600

    Create status modal

--- screens/ChangeStatusModal.js
+++ screens/ChangeStatusModal.js
@@ -1,12 +1,89 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import StatusBadge from '../components/StatusBadge';
+import VolunteerContext from '../volunteer-context';
 
-const ChangeStatusModal = ({ route }) => {
+const ChangeStatusModal = ({ route, navigation: { goBack } }) => {
+  const { assignedLists, setAssignedLists } = useContext(VolunteerContext);
+  const item = route.params.item;
+
+  const handlePress = (status) => {
+    item.status = status;
+    let lists = [...assignedLists];
+    let selectedList = lists.find((list) => {
+      return list.id === item.id;
+    });
+    let index = assignedLists.indexOf(selectedList);
+    lists[index].status = status;
+    setAssignedLists(lists);
+    goBack();
+  };
   return (
-    <View>
-      <Text>{route.params.item.status}</Text>
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <View>
+          <Text style={styles.title}>Change status</Text>
+        </View>
+        <StatusBadge
+          status="pending"
+          customStyles={{ marginTop: 10 }}
+          highlighted={item.status === 'pending'}
+          onPress={() => {
+            handlePress('pending');
+          }}
+        />
+        <StatusBadge
+          status="assigned"
+          customStyles={{ marginTop: 10 }}
+          highlighted={item.status === 'assigned'}
+          onPress={() => {
+            handlePress('assigned');
+          }}
+        />
+        <StatusBadge
+          status="at store"
+          customStyles={{ marginTop: 10 }}
+          highlighted={item.status === 'at store'}
+          onPress={() => {
+            handlePress('at store');
+          }}
+        />
+        <StatusBadge
+          status="on way"
+          customStyles={{ marginTop: 10 }}
+          highlighted={item.status === 'on way'}
+          onPress={() => {
+            handlePress('on way');
+          }}
+        />
+        <StatusBadge
+          status="delivered"
+          customStyles={{ marginTop: 10 }}
+          highlighted={item.status === 'delivered'}
+          onPress={() => {
+            handlePress('delivered');
+          }}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  innerContainer: {
+    marginBottom: 100,
+  },
+});
 
 export default ChangeStatusModal;
