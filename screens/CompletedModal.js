4acc97315a7770d0f67e0ea910da6384227d66e4
Edward Stoner <edwardlstoner@gmail.com>
2020-05-31 15:55:00 -0600

    Add completed modal

---
+++ screens/CompletedModal.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { MaterialIcons } from 'react-native-vector-icons';
+import { Button } from '../components/Button';
+
+const CompletedModal = ({ navigation: { goBack } }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <View>
+          <MaterialIcons name="done" color="green" size={120} />
+        </View>
+        <Text style={styles.introtext}>
+          You have acquired all items for this list!
+        </Text>
+        <Button
+          text="CLOSE"
+          customStyles={{ backgroundColor: '#59DE7E', marginTop: 20 }}
+          onPress={() => goBack()}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerContainer: {
+    width: 300,
+    marginBottom: 50,
+    alignItems: 'center',
+  },
+  introtext: {
+    fontSize: 30,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+});
+
+export default CompletedModal;
