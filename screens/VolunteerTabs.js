ad32cf4baeb0db4a24e7c6219db62a9184f52611
Edward Stoner <edwardlstoner@gmail.com>
2020-05-31 15:39:51 -0600

    Add badge

--- screens/VolunteerTabs.js
+++ screens/VolunteerTabs.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { View, Text } from 'react-native';
 import VolunteerContext from '../volunteer-context';
 import { MaterialIcons } from 'react-native-vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -7,11 +8,52 @@ import VolunteerTask from './VolunteerTask';
 
 const Tab = createBottomTabNavigator();
 
-const VolunteerTabs = ({ route, navigation }) => {
-  const { findSingleList } = useContext(VolunteerContext);
+const VolunteerTabs = ({ navigation, route }) => {
+  const { singleList, assignedLists } = useContext(VolunteerContext);
 
-  const { params } = route;
-  findSingleList(params);
+  const list = assignedLists[route.params.params.selectedList];
+  console.log(list);
+
+  let itemsLeft = list.items.filter(
+    (item) => !item.acquired && !item.unavailable,
+  );
+  let totalItemsLeft = itemsLeft.reduce((acc, el) => {
+    acc += el.quantity;
+    return acc;
+  }, 0);
+  function IconWithBadge({ name, badgeCount, color, size }) {
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <MaterialIcons name={name} size={size} color={color} />
+        {badgeCount > 0 && (
+          <View
+            style={{
+              // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+              position: 'absolute',
+              right: -6,
+              top: -3,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+              {badgeCount}
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+
+  function HomeIconWithBadge(props) {
+    // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+    return <IconWithBadge {...props} badgeCount={totalItemsLeft} />;
+  }
+
   // console.log(params.selectedList.listId);
   return (
     <Tab.Navigator>
@@ -21,7 +63,11 @@ const VolunteerTabs = ({ route, navigation }) => {
         options={{
           tabBarLabel: 'Shop',
           tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="shopping-basket" color={color} size={size} />
+            <HomeIconWithBadge
+              name="shopping-basket"
+              color={color}
+              size={size}
+            />
           ),
         }}
       />
