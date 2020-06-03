88896fac73cc9ddec2224e4c196d1752509e5c9b
monstaro <codysmith2814@gmail.com>
2020-06-03 08:58:51 -0600

    Fixes bug

--- components/TimeOfDay.js
+++ components/TimeOfDay.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import { View, Image, StyleSheet, Text } from 'react-native';
-
 const TimeOfDay = () => {
   let today = new Date();
   let hour = today.getHours();
@@ -8,28 +5,13 @@ const TimeOfDay = () => {
   if (hour < 12) {
     greeting = 'Good morning';
   }
-  if (hour > 18) {
+  if (hour >= 18) {
     greeting = 'Good evening';
-  } else {
+  }
+  if (hour >= 12 && hour < 18) {
     greeting = 'Good afternoon';
   }
   return greeting;
 };
-
-const styles = StyleSheet.create({
-  logoContainer: {
-    alignItems: 'center',
-  },
-  logo: {
-    marginTop: 20,
-    height: 99,
-    width: 105,
-  },
-  logoText: {
-    color: '#59DE7E',
-    fontFamily: 'HelveticaNeue-Bold',
-    fontSize: 24,
-  },
-});
 
 export default TimeOfDay;
