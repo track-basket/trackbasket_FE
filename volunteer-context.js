5a474ae1ada35d0fdf0c7492e4974eea3a122f65
Edward Stoner <edwardlstoner@gmail.com>
2020-06-04 14:19:38 -0600

    Remove comments

--- volunteer-context.js
+++ volunteer-context.js
@@ -1,12 +1,6 @@
 import React from 'react';
 
-const VolunteerContext = React.createContext({
-  //   user: { name: 'ed' },
-  //   cart: [],
-  //   location: {},
-  //   addToCart: () => {},
-  //   setUser: () => {},
-});
+const VolunteerContext = React.createContext({});
 
 export const VolunteerProvider = VolunteerContext.Provider;
 export const VolunteerConsumer = VolunteerContext.Consumer;
