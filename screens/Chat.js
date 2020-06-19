27ec9f93ead4e8d4d310b447e45fa4664bea7cb8
Edward Stoner <edwardlstoner@gmail.com>
2020-06-19 14:12:51 -0600

    Add chat functionality

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import VolunteerContext from '../volunteer-context';
+import io from 'socket.io-client';
+let socket;
 
 const Chat = () => {
+  const { singleList, volunteer } = useContext(VolunteerContext);
+
+  const [allMessages, setAllMessages] = useState([]);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    socket = io('http://10.3.13.6:3000');
+    socket.emit('joinRoom', singleList.id);
+    socket.on('chat message', (msg) => {
+      setAllMessages((allMessages) => [...allMessages, msg]);
+    });
+  }, []);
+
+  const chatMessages = allMessages.map((chatMessage) => {
+    return <Text key={chatMessage + Date.now()}>{chatMessage}</Text>;
+  });
+
+  const submitChatMessage = () => {
+    socket.emit('chat message', volunteer.name + ': ' + message);
+
+    setMessage('');
+  };
+
   return (
     <View>
-      <Text>Hey</Text>
+      <TextInput
+        style={{ height: 40, borderWidth: 2 }}
+        value={message}
+        onSubmitEditing={() => submitChatMessage()}
+        autoCorrect={false}
+        onChangeText={(chatMessage) => {
+          setMessage(chatMessage);
+        }}
+      />
+      <Text>{`Chat about ${singleList.userDetails.name}'s groceries`}</Text>
+      {chatMessages}
     </View>
   );
 };
