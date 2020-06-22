81e0d61a5d2c4367df57aa5fef336db5c0eca1ef
Edward Stoner <edwardlstoner@gmail.com>
2020-06-21 22:41:42 -0600

    Style chat

--- screens/AtRiskChat.js
+++ screens/AtRiskChat.js
@@ -3,11 +3,24 @@ import { View, Text, TextInput, StyleSheet } from 'react-native';
 import UserContext from '../user-context';
 
 const AtRiskChat = () => {
-  const { allMessages, setNewMessage } = useContext(UserContext);
+  const { allMessages, setNewMessage, user } = useContext(UserContext);
   const [message, setMessage] = useState('');
-
+  console.log(user.name);
   const chatMessages = allMessages.map((chatMessage, i) => {
-    return <Text key={i}>{chatMessage}</Text>;
+    console.log(chatMessage.split(': ')[0]);
+
+    return (
+      <Text
+        key={i}
+        style={
+          chatMessage.split(': ')[0] === user.name
+            ? styles.msgLeft
+            : styles.msgRight
+        }
+      >
+        {chatMessage}
+      </Text>
+    );
   });
 
   const submitChatMessage = () => {
@@ -16,20 +29,56 @@ const AtRiskChat = () => {
   };
 
   return (
-    <View>
-      <TextInput
-        style={{ height: 40, borderWidth: 2 }}
-        value={message}
-        onSubmitEditing={() => submitChatMessage()}
-        autoCorrect={false}
-        onChangeText={(chatMessage) => {
-          setMessage(chatMessage);
-        }}
-      />
-      <Text>{`Chat with your volunteer shopper`}</Text>
-      {chatMessages}
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text style={styles.title}>{`Chat with your volunteer shopper`}</Text>
+        {chatMessages}
+
+        <TextInput
+          style={styles.textInput}
+          value={message}
+          onSubmitEditing={() => submitChatMessage()}
+          autoCorrect={false}
+          onChangeText={(chatMessage) => {
+            setMessage(chatMessage);
+          }}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: 'white',
+    marginTop: 0,
+  },
+  innerContainer: {
+    alignItems: 'center',
+    width: 400,
+    marginTop: 100,
+  },
+  textInput: {
+    width: 300,
+    borderWidth: 2,
+    height: 40,
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 20,
+  },
+  msgLeft: {
+    fontSize: 18,
+    alignSelf: 'flex-start',
+    marginTop: 20,
+  },
+  msgRight: {
+    fontSize: 18,
+    alignSelf: 'flex-end',
+    marginTop: 20,
+  },
+});
 
 export default AtRiskChat;
